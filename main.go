a79861bb40591b479351844d9810507679ce425c
Steve McCarthy <steve@redlua.com>
2017-07-04 14:31:49 -0400

    Setup project to be run on a server
    Previously the project required one to manually copy and enter an
    offline token because it was not expecting to have access to a
    RedirectURL.
    Now it expects to have a RedirectURL, and is setup accordingly.
    
    This commit removes a lot of cruft and creates two routes that properly
    handle logging in - one that redirects to an OAuth2 endpoint and another
    which handles being the OAuth2 RedirectURL endpoint.
    
    Also, tokens are now persisted using an in memory session library.

--- main.go
+++ main.go
@@ -11,21 +11,38 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
-	"golang.org/x/oauth2"
-
+	"github.com/alexedwards/scs/engine/memstore"
+	"github.com/alexedwards/scs/session"
 	"github.com/ifo/oauth2rc"
 	"github.com/pressly/chi"
 	"github.com/pressly/chi/middleware"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	var port = flag.Int("port", 3000, "Port to run the server on")
+	portStr := os.Getenv("PORT")
+	portDefault := 3000
+	var err error
+	if portStr != "" {
+		portDefault, err = strconv.Atoi(portStr)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	var port = flag.Int("port", portDefault, "Port to run the server on")
 
 	flag.Parse()
 
+	engine := memstore.New(5 * time.Minute)
+	sessionManager := session.Manage(engine)
+
 	r := chi.NewRouter()
 
+	// TODO: Audit middleware
+	// TODO: Add Auth middleware
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
@@ -35,18 +52,16 @@ func main() {
 	r.FileServer("/assets/", http.Dir("assets"))
 
 	// Routes
+	// TODO: Separate authed routes from unauthed routes
+	// TODO: Add logout
 	r.Get("/", indexHandler)
+	r.Get("/rc/login", loginHandler)
+	r.Get("/rc/redirect", redirectHandler)
 
-	// Cleanup or Remove
+	// Remove later
 	r.Get("/rc/me", rcHandler)
-	r.Get("/rc/token", goToTokenHandler)
-	r.Get("/rc/enter", enterTokenHandler)
-	r.Post("/rc/enter", submitTokenHandler)
 
-	// Helper functions. Remove later.
-	r.Get("/headers", headersHandler)
-
-	http.ListenAndServe(fmt.Sprintf(":%d", *port), r)
+	http.ListenAndServe(fmt.Sprintf(":%d", *port), sessionManager(r))
 }
 
 /*
@@ -58,24 +73,123 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+// Start the OAuth2 process.
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("REDIRECT_URL"),
+		Endpoint:     oauth2rc.Endpoint,
+	}
+
+	// Redirect user to consent page to ask for permission for this app.
+	// TODO: randomize state
+	url := conf.AuthCodeURL("state", oauth2.AccessTypeOnline)
+	http.Redirect(w, r, url, 302)
+}
+
+// Finish the OAuth2 process.
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: check state
+	code := r.FormValue("code")
+	if code == "" {
+		http.Error(w, "no token received", 500)
+		return
+	}
+
+	ctx := context.Background()
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("CLIENT_ID"),
+		ClientSecret: os.Getenv("CLIENT_SECRET"),
+		RedirectURL:  os.Getenv("REDIRECT_URL"),
+		Endpoint:     oauth2rc.Endpoint,
+	}
+
+	token, err := conf.Exchange(ctx, code)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// Regenerate the session.
+	err = session.RegenerateToken(r)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	err = saveToken(r, token)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// TODO: keep track of where to redirect the user
+	http.Redirect(w, r, "/", 302)
+}
+
+/*
+// Helpers
+*/
+
+func saveToken(r *http.Request, t *oauth2.Token) error {
+	if err := session.PutString(r, "accesstoken", t.AccessToken); err != nil {
+		return err
+	}
+	if err := session.PutString(r, "tokentype", t.TokenType); err != nil {
+		return err
+	}
+	if err := session.PutString(r, "refreshtoken", t.RefreshToken); err != nil {
+		return err
+	}
+	if err := session.PutTime(r, "expiry", t.Expiry); err != nil {
+		return err
+	}
+	return nil
+}
+
+func getToken(r *http.Request) (*oauth2.Token, error) {
+	token := &oauth2.Token{}
+	if at, err := session.GetString(r, "accesstoken"); err != nil {
+		return nil, err
+	} else {
+		if at == "" { // We don't have a token yet
+			return nil, nil
+		}
+		token.AccessToken = at
+	}
+	if tt, err := session.GetString(r, "tokentype"); err != nil {
+		return nil, err
+	} else {
+		token.TokenType = tt
+	}
+	if rt, err := session.GetString(r, "refreshtoken"); err != nil {
+		return nil, err
+	} else {
+		token.RefreshToken = rt
+	}
+	if exp, err := session.GetTime(r, "expiry"); err != nil {
+		return nil, err
+	} else {
+		token.Expiry = exp
+	}
+
+	return token, nil
+}
+
 /*
 // Cleanup or remove later
 */
 
-// global var for the token string.
-var token *oauth2.Token
-
-// See what headers come through.
-func headersHandler(w http.ResponseWriter, r *http.Request) {
-	for k, v := range r.Header {
-		fmt.Fprintln(w, k, v)
-	}
-}
-
 // Get rc/me using a saved token.
 func rcHandler(w http.ResponseWriter, r *http.Request) {
+	token, err := getToken(r)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
 	if token == nil {
-		http.Redirect(w, r, "/rc/token", 302)
+		http.Redirect(w, r, "/rc/login", 302)
 		return
 	}
 	// We have a token now.
@@ -105,72 +219,4 @@ func rcHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Write(jsonBody.Bytes())
-}
-
-func goToTokenHandler(w http.ResponseWriter, r *http.Request) {
-	conf := &oauth2.Config{
-		ClientID:     os.Getenv("CLIENT_ID"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("REDIRECT_URL"),
-		Endpoint:     oauth2rc.Endpoint,
-	}
-
-	// Redirect user to consent page to ask for permission for this app.
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	http.Redirect(w, r, url, 302)
-}
-
-func enterTokenHandler(w http.ResponseWriter, r *http.Request) {
-	tokenForm := `<!DOCTYPE html>
-<html>
-  <head>
-    <link rel="stylesheet" href="/assets/css/tachyons.min.css" />
-    <link rel="stylesheet" href="/assets/css/site.css" />
-  </head>
-  <body>
-    <header>
-      <div class="tc w-100 w-50-ns">
-        <h1>OSS Reading Club</h1>
-      </div>
-    </header>
-    <main>
-      <p>Submit your token:</p>
-      <form action='/rc/enter' method='post'>
-        <input type='text' name='token' />
-				<button type='submit'>Submit</button>
-      </form>
-    </main>
-  </body>
-</html>`
-	tmpl := template.Must(template.New("tokenform").Parse(tokenForm))
-	tmpl.Execute(w, nil)
-}
-
-func submitTokenHandler(w http.ResponseWriter, r *http.Request) {
-	code := r.PostFormValue("token")
-	if code == "" {
-		w.WriteHeader(500)
-		w.Write([]byte("no token received"))
-		return
-	}
-
-	ctx := context.Background()
-	conf := &oauth2.Config{
-		ClientID:     os.Getenv("CLIENT_ID"),
-		ClientSecret: os.Getenv("CLIENT_SECRET"),
-		RedirectURL:  os.Getenv("REDIRECT_URL"),
-		Endpoint:     oauth2rc.Endpoint,
-	}
-
-	tok, err := conf.Exchange(ctx, code)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	// Save the global token.
-	token = tok
-
-	http.Redirect(w, r, "/rc/me", 302)
 }
